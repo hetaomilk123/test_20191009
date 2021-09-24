c12c87b3d5bb35ebcf4803d356449c5fe05daacc
hetaomilk123 <hetaomilk123@126.com>
2021-09-24 17:50:02 +0800

    modify files

---
+++ groups/frt/frta/frta_apple.cpp
@@ -0,0 +1,14 @@
+// frta_apple.cpp                                                     -*-C++-*-
+#include <frta_apple.h>
+
+namespace HetaoMI {
+namespace frta {
+
+
+///Implementation Notes
+///--------------------
+// . . . notes go here
+
+
+}  // close package namespace
+}  // close enterprise namespace
